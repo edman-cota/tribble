c7cfcfd5f5317efdaa42349327c0864350438b59
edman-cota <edmancota2017@gmail.com>
2022-11-30 20:58:46 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const cors = require('cors');
+const routerApi = require('./routes');
+
+const {
+  logErrors,
+  errorHandler,
+  boomErrorHandler,
+} = require('./middlewares/error.handler');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+const whiteList = ['http://127.0.0.1:5173'];
+const options = {
+  origin: (origin, callback) => {
+    if (whiteList.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('No permitido'));
+    }
+  },
+};
+app.use(cors(options));
+
+app.get('/', (req, res) => {
+  res.send('Hola mi server en express');
+});
+
+routerApi(app);
+
+app.use(logErrors);
+app.use(boomErrorHandler);
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log('Running', port);
+});
